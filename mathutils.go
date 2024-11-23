6cad65a61e7150af8f2452256c9234d3b2430c1e
khazar <x.talibov@mia.gov.az>
2024-11-23 10:55:39 +0400

    inital

---
+++ mathutils.go
@@ -0,0 +1,11 @@
+package mathutils
+
+// Add adds two integers and returns the result.
+func Add(a int, b int) int {
+	return a + b
+}
+
+// Multiply multiplies two integers and returns the result.
+func Multiply(a int, b int) int {
+	return a * b
+}
